76c2162c767007234894589f0fc853ba04fad034
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-03-15 13:46:30 +0000

    Update DTime.h

--- DTime.h
+++ DTime.h
@@ -7,13 +7,6 @@
 #include "WProgram.h"
 #endif
 
-#define DTIME_DAYS_UNTIL(y) ({((((y) - 1) * 365UL) + (((y) - 1) / 4) - (((y) - 1) / 100) + (((y) - 1) / 400));})
-#define DTIME_DAYS_BETWEEN_YEARS(y1,y2) ({(DTIME_DAYS_UNTIL(((y1) > (y2)) ? (y1) : (y2)) - DTIME_DAYS_UNTIL(((y1) > (y2)) ? (y2) : (y1)));})
-#define DTIME_LEAP_YEAR(y) ({!(((y) % 4) * (!((y) % 100) + ((y) % 400)));})
-#define DTIME_MONTH_DAYS(y,m) ({(31 - (((m) > 2) ? ((((m) - 1) - (5 * ((m) > 7))) % 2) : (((m) - 1) * (3 - DTIME_LEAP_YEAR(y)))));})
-#define DTIME_YEAR_DAYS_UNTIL(y,m) ({uint16_t d = 0; for(uint8_t i = 1; i < (m); i++) d += DTIME_MONTH_DAYS((y), i); d;})
-#define DTIME_WEEKDAY(y,m,d) ({(DTIME_DAYS_UNTIL(y) + DTIME_YEAR_DAYS_UNTIL((y), (m)) + (d)) % 7;})
-
 class DTime {
   public:
     const uint8_t &month = _month, &weekday = _weekday, &day = _day, &hour = _hour, &minute = _minute, &second = _second;
